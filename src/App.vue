b7f81d60a7c4385591dedbaa516bb14dcb9efd18
Gonciarov <gonciarov@yandex.ru>
2021-02-23 13:18:08 +0000

    empty vue project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Hello C4K student! Feel free to Vue anything here!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
